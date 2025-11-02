a24eb30e537609042115114dcfdbfd663c866053
Jack17kdb <jack@kali.Johnson.com>
2025-11-02 16:53:10 +0300

    s3 bucket creation

---
+++ tf-aws-s3/main.tf
@@ -0,0 +1,22 @@
+terraform {
+   required_providers {
+      aws = {
+         source = "hashicorp/aws"
+         version = "~> 5.0"
+      }
+   }
+
+   required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+   bucket = "my_terraform_bucket"
+   tags = {
+      Name = "Terraform Bucket"
+      Environment = "Dev"
+   }
+}
